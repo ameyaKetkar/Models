f8a67ae014f4b6c25586959275f9c1bd229f81f6
Ameya Ketkar <ketkara@oregonstate.edu>
2020-01-10 18:59:42 -0800

    Html templates

--- Models/ToHtml.py
+++ Models/ToHtml.py
@@ -2,55 +2,57 @@ import RW
 from jinja2 import Environment, FileSystemLoader
 import os
 
-
-
-
-
 fileDir = os.path.dirname(os.path.realpath('__file__'))
 pathToProjectsHtml = os.path.join(fileDir, "../../docs/projects.html")
 
-
 env = Environment(loader=FileSystemLoader(fileDir))
 template = env.get_template("ProjectTemplate.html")
-commmitTemplate = env.get_template("CommitTemplate.html")
-
+commmitTemplate = env.get_template("CommitSummaryTemplate.html")
+detailedCommmitTemplate = env.get_template("DetailCommitTemplate.html")
 
 projects = RW.readProject('projects')
 items = []
 for p in projects:
     commits = RW.readCommit('commits_' + p.name)
     l = str(len(commits))
-    d = dict(name = p.name, Url = p.url, totalCommits=p.totalCommits, CommitsAnalyzed = l)
+    d = dict(name=p.name, Url=p.url, totalCommits=p.totalCommits, CommitsAnalyzed=l,
+             LinkToCommits=p.name + ".html")
     items.append(d)
-    cmts = []
+    commitSummary = []
     for cmt in commits:
-        r = str(sum(list(map(lambda r: r.occurences, cmt.refactorings))))
-        c = dict(sha = cmt.sha, filesAdded = cmt.fileDiff.filesAdded,
-                 filesRenamed = cmt.fileDiff.filesRenamed,
-                 filesModified = cmt.fileDiff.filesModified,
-                 filesRemoved = cmt.fileDiff.filesRemoved,
-                 noOfJars = str(len(cmt.dependencies)),
-                 refactoringsLink = cmt.sha + ".html",
-                 noOfRefactoring = r)
-        cmts.append(c)
-        # for ref in cmt.refactorings:
+        r = sum(list(map(lambda r: r.occurences, cmt.refactorings)))
+        refactorings = []
+        dependencies = []
 
+        commitSummary.append(dict(sha=cmt.sha, noOfJars=str(len(cmt.dependencies)),
+                                  refactoringsLink= p.name + "/" + cmt.sha + ".html" if r > 0 else None,
+                                  noOfRefactoring=str(r),
+                                  typeChangeFound='Yes' if cmt.isTypeChangeReported else 'No',
+                                  isException='Yes' if (cmt.exception is not '') else 'No'))
 
+        for ref in cmt.refactorings:
+            refactorings.append(dict(name=ref.name, occurence=ref.occurences))
+        for dep in cmt.dependencies:
+            dependencies.append(dict(name=dep.artifactID+":"+dep.groupID+":"+dep.version))
 
+        if len(refactorings) > 0:
+            pathToCommitsInProject = os.path.join(fileDir, "../../docs/" + p.name)
+            pathToDetailedCommits = os.path.join(pathToCommitsInProject, cmt.sha + ".html")
+            os.makedirs(os.path.dirname(pathToDetailedCommits), exist_ok=True)
+            with open(pathToDetailedCommits, 'w+') as fh:
+                fh.write(detailedCommmitTemplate.render(sha=cmt.sha, filesAdded=cmt.fileDiff.filesAdded,
+                                                        filesRemoved=cmt.fileDiff.filesRemoved,
+                                                        filesRenamed=cmt.fileDiff.filesRenamed,
+                                                        filesModiefied=cmt.fileDiff.filesModified,
+                                                        refactorings=refactorings,
+                                                        dependencies=dependencies))
+                fh.write('\n')
 
-
-
-    pathToProjectCommits =  os.path.join(fileDir, "../../docs/" + p.name +".html")
+    pathToProjectCommits = os.path.join(fileDir, "../../docs/" + p.name + ".html")
     with open(pathToProjectCommits, 'a') as fh:
-        fh.write(commmitTemplate.render(projectName = p.name, commits=cmts))
+        fh.write(commmitTemplate.render(projectName=p.name, commits=commitSummary))
         fh.write('\n')
-
-
-
-
 
 with open(pathToProjectsHtml, 'a') as fh:
     fh.write(template.render(projects=items))
     fh.write('\n')
-
-
