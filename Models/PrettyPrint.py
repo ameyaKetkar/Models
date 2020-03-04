201416934a47bf8d8b0fa088fba07a32c150648d
Ameya <ketkara@oegonstate.edu>
2020-03-03 20:16:02 -0800

    verification py

--- Models/PrettyPrint.py
+++ Models/PrettyPrint.py
@@ -22,6 +22,8 @@ def pretty(t: TypeGraph) -> str:
             if "super" in k:
                 bound = ("super" + pretty(v))
             return "? " + bound
+        if len(t.edges.items()) == 0:
+            return "?"
     if t.root.kind == t.root.Array:
         return pretty(t.edges['of']) + "[]"
     if t.root.kind == t.root.Union or t.root.kind == t.root.Intersection:
@@ -43,3 +45,43 @@ def prettyNameSpace(n: int) -> str:
         return "Internal"
     if n == 5:
         return "DontKnow"
+
+def prettyNameSpace1(n: int) -> str:
+    if n == 0:
+        return "TypeVariable"
+    if n == 2:
+        return "Jdk"
+    if n == 3:
+        return "External"
+    if n == 4:
+        return "Internal"
+    if n == 5:
+        return "External"
+
+
+def prettyElementKind(n: int) -> str:
+    if n == 0:
+        return "Field"
+    if n == 2:
+        return "LocalVariable"
+    if n == 3:
+        return "Parameter"
+    if n == 4:
+        return "Return"
+
+
+def prettyTypeKind(n: int) -> str:
+    if n == 0:
+        return "Simple"
+    if n == 2:
+        return "Parameterized"
+    if n == 3:
+        return "WildCard"
+    if n == 4:
+        return "Union"
+    if n == 5:
+        return "Primitive"
+    if n == 6:
+        return "Intersection"
+    if n == 7:
+        return "Array"
